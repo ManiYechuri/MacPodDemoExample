07472506ee743efe753f2b08288c282982de0bee
Mani Yechuri <maniyechuri@Manis-MacBook-Air.local>
2023-07-17 11:38:35 +0200

    Added test cases

---
+++ MacPodDemo1/MacPodDemo.swift
@@ -0,0 +1,22 @@
+//
+//  MacPodDemo.swift
+//  MacPodDemo1
+//
+//  Created by Mani Yechuri on 2023/07/17.
+//
+
+import Foundation
+
+public final class MacPodDemo {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a:Int, b:Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a:Int, b:Int) -> Int {
+        return a - b
+    }
+    
+}
--- MacPodDemo1Tests/MacPodDemo1Tests.swift
+++ MacPodDemo1Tests/MacPodDemo1Tests.swift
@@ -9,28 +9,33 @@ import XCTest
 @testable import MacPodDemo1
 
 final class MacPodDemo1Tests: XCTestCase {
+    
+    var macPodDemo : MacPodDemo!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        macPodDemo = MacPodDemo()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            
         }
+    }
+    
+    func testAddIsTrue(){
+        XCTAssertEqual(macPodDemo.add(a:1, b: 2), 3)
+    }
+    
+    func testIsSubTrue(){
+        XCTAssertEqual(macPodDemo.sub(a: 1, b: 1), 0)
     }
 
 }
