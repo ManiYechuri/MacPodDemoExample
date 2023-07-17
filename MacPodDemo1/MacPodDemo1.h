29038e317ef83db448b398110204802300c0f73d
Mani Yechuri <maniyechuri@Manis-MacBook-Air.local>
2023-07-17 10:48:54 +0200

    This is first commit to pod demo

---
+++ MacPodDemo1/MacPodDemo1.h
@@ -0,0 +1,18 @@
+//
+//  MacPodDemo1.h
+//  MacPodDemo1
+//
+//  Created by Mani Yechuri on 2023/07/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MacPodDemo1.
+FOUNDATION_EXPORT double MacPodDemo1VersionNumber;
+
+//! Project version string for MacPodDemo1.
+FOUNDATION_EXPORT const unsigned char MacPodDemo1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MacPodDemo1/PublicHeader.h>
+
+
